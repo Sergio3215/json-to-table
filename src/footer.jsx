f6500c8924ef5e8413b9831911849d1012f8e70a
Sergio <principiante.en.programar@gmail.com>
2025-03-28 19:30:34 -0300

    add copyright and change name

---
+++ src/footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return(
+        <footer>
+            Derechos Reservados&copy; {new Date().getFullYear()} - <a href="https://www.serez.dev/">Serez Dev</a>
+        </footer>
+    )
+}
